4ecd81edbf5fde335def81a107d1520c8f1542ed
Artem Dorodnykh <dorodnykhartem@gmail.com>
2024-08-13 17:57:26 +0300

    add js test file

---
+++ jstest.js
@@ -0,0 +1,19 @@
+let Kakish = 10
+
+console.log(Kakish)
+
+let baba
+
+console.log(baba)
+
+baba = 'babka'
+
+console.log(baba)
+
+let hui
+
+console.log(console.log (hui))
+
+const artem = 'temochka'
+
+console.log(artem)
