ce5ddd848d1430368aafeb0c670c8a5c53dd4276
Artem Dorodnykh <dorodnykhartem@gmail.com>
2024-08-15 12:58:06 +0300

    new changes in JS file

---
+++ jstest.js
@@ -0,0 +1,34 @@
+let Kakish = 10
+
+console.log(Kakish)
+
+let baba
+
+console.log(baba)
+
+baba = 'babka'
+
+console.log(baba)
+
+let hui
+
+console.log(console.log (hui))
+
+const artem = 'temochka'
+
+console.log(artem)
+
+const object = {
+    a: 15,
+    b: 20
+}
+
+console.log(object)
+
+const copyOfObject = object
+
+copyOfObject.c = '30'
+
+console.log(copyOfObject)
+
+const dd = 5
