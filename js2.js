12573436b62a976831b34bb92062d9cb777d5d1f
Artem Dorodnykh <dorodnykhartem@gmail.com>
2024-08-19 19:32:10 +0300

    add new file

---
+++ js2.js
@@ -0,0 +1,12 @@
+// тот файл был уже слишком большим так что я создал новый
+const object= {
+    sv1: '123',
+    sv2: '246',
+    sbornik: {
+        f1: 'Nigga rap',
+        f2: 12
+    }
+}
+console.log(object)
+
+console.log(object.sbornik.f1)
