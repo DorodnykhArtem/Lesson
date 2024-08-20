512a3817dac413f29fd3bfaf6ee2b19ab5697a91
Artem Dorodnykh <dorodnykhartem@gmail.com>
2024-08-20 19:28:30 +0300

    update with information from new video lessons

--- js2.js
+++ js2.js
@@ -10,3 +10,16 @@ const object= {
 console.log(object)
 
 console.log(object.sbornik.f1)
+// УРОК 15
+const da = 'PIZ'
+const object1 = true
+
+const dunduk = {
+    object1,
+    da,
+    function: 'svoistvo'
+// Я запомнил что нужно сначала писать более короткое свойство но тут я так сделал что бы посмотреть изменится ли порядок
+}
+
+console.log(dunduk)
+// УРОК 16
