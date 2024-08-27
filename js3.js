fc6e24d2891faea599bca32a08de7d2eaa332615
Artem Dorodnykh <dorodnykhartem@gmail.com>
2024-08-27 14:24:51 +0300

    Add new JS3 file

---
+++ js3.js
@@ -0,0 +1,25 @@
+// УРОК 19
+const a = 5
+
+let b = a
+
+b = 22
+
+console.log(a)
+
+console.log(b)
+
+БАЗА_ЦРУ = "БАЗА ЦРУ"
+console.log(БАЗА_ЦРУ)
+
+const Base1 = {
+    person: "Pablo Escobar",
+    Criminal_Type: "Drug Dealer",
+    Age: "False"
+    // (Killed)
+}
+
+const Base2 = Base1
+
+Base2.Third_Variable = "AAA"
+console.log(Base2)
