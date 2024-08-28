51ef0652064f4a6eeabfbacccd61dad0bb87a9be
Artem Dorodnykh <dorodnykhartem@gmail.com>
2024-08-28 18:54:54 +0300

    all lessons to 20th

--- js3.js
+++ js3.js
@@ -23,3 +23,24 @@ const Base2 = Base1
 
 Base2.Third_Variable = "AAA"
 console.log(Base2)
+
+const Base3 = Object.assign ({}, Base1) 
+Base3.person = "Al Capone"
+Base3.Age = "False"
+Base3.Criminal_Type = "Gangster"
+
+console.log(Base3)
+
+const Base4 = {... Base1}
+Base4.person = "Doku Umarov"
+Base4.Age = 60
+Base4.Criminal_Type = "Terrorist"
+
+console.log(Base4)
+console.log(Base1.person)
+
+const Base5 = JSON.parse(JSON.stringify(Base2))
+
+Base5.person = "Мне лень менять"
+Base5.Age = "Мне лень менять"
+Base5.Criminal_Type = "Мне лень менять"
