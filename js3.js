2c9e4028db1feb7d2e2cd57567b3a5531211eb77
Artem Dorodnykh <dorodnykhartem@gmail.com>
2024-09-02 13:29:58 +0300

    finish fil3

--- js3.js
+++ js3.js
@@ -44,3 +44,41 @@ const Base5 = JSON.parse(JSON.stringify(Base2))
 Base5.person = "Мне лень менять"
 Base5.Age = "Мне лень менять"
 Base5.Criminal_Type = "Мне лень менять"
+
+// УРОК 22
+
+function myFunction (a, b) {
+    let c
+    a = a + 1
+    c = a + b
+    return c
+}
+
+console.log(myFunction (5, 25))
+console.log(myFunction (7, 8))
+// Я уже экспериментировал с тем что если не добавлять "return"
+
+// УРОК 23
+const penis = {
+    fimoz: true,
+    length: 19
+}
+console.log(penis)
+function test (отсекация) {
+    penis.length -= 12,
+    penis.fimoz = "отрезан"
+    return отсекация
+}
+
+test(penis)
+console.log(penis)
+
+function test2 (отсекация) {
+    const newots = Object.assign ( {}, отсекация) 
+    newots.length -= 13
+    return newots
+}
+
+const otsek = test2(penis)
+console.log(penis)
+console.log(otsek)
