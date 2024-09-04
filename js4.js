d34217f2c4b6a9b34e8fd221f5891a91fbce9743
Artem Dorodnykh <dorodnykhartem@gmail.com>
2024-09-04 12:32:47 +0300

    FINAL

---
+++ js4.js
@@ -0,0 +1,13 @@
+function timeSet2 () {
+    console.log("2 seconds")
+}
+
+setTimeout (timeSet2, 2000)
+
+function timeSet () {
+    console.log("10 seconds")
+}
+
+setTimeout (timeSet, 10000)
+
+// УРОК 25 ФИНАЛ!!!
